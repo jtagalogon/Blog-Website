2139be4cf9c6370e5b9c7b9b625ab6c447806cbd
jer04 <35080681+jer04@users.noreply.github.com>
2019-07-17 12:17:57 -0700

    Add files via upload

--- media/models.py
+++ media/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-class Media(models.Model):
-    media_id = models.IntegerField(primary_key=True)
-    media = models.ImageField(blank=True)
-# Is this a many-to-many relationship? for users/profile_picture & post/media?
-
 # Create your models here.
