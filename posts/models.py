f9c39a5dfdebef81b8c2d2b77fbb7735a72263b2
jer04 <35080681+jer04@users.noreply.github.com>
2019-07-17 12:16:34 -0700

    Add files via upload

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,9 @@ class Post(models.Model):
     post_id = models.IntegerField(primary_key=True)
     post_title = models.CharField(max_length=100)
     post_body = models.TextField()
-    likes = models.IntegerField(blank=True)
-    dislikes = models.IntegerField(blank=True)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
-    post = models.ForeignKey(Users, on_delete=models.CASCADE)
-# Does it matter where this 'poster' foreign key goes?
 
     def __str__(self):
     	return self.post_title
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from .models import Post
 
 
 # Create your views here.
+
 def index(request):
-	
+
 	return render(request, 'posts/index.html')
